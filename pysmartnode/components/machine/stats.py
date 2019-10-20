df739156d5cfa77806cf286a4dbc757afbc555a2
Kevin Köck <kevinkk525@users.noreply.github.com>
2019-10-20 15:49:56 +0200

    Major STATS updated, publishing many device stats as hass json_attributes and RSSI as main value instead of "online/offline" which is now done directly in mqtt as availability topic.
    - last_boot
    - uptime
    - pysmartnode version
    - micropython firmware version
    - RAM free
    - RSSI
    - MQTT Downtime
    - MQTT Reconnects
    - Asyncio waitq used size

--- pysmartnode/components/machine/stats.py
+++ pysmartnode/components/machine/stats.py
@@ -5,17 +5,23 @@
 # This component will be started automatically to provide basic device statistics.
 # You don't need to configure it to be active.
 
-__updated__ = "2019-10-16"
-__version__ = "0.7"
+__updated__ = "2019-10-20"
+__version__ = "1.0"
 
 import gc
 
 from pysmartnode import config
 import uasyncio as asyncio
-from pysmartnode.utils.component import Component, TIMELAPSE_TYPE, DISCOVERY_SENSOR
+from pysmartnode.utils.component import Component
 import time
 from sys import platform
 from pysmartnode import logging
+from pysmartnode.utils import sys_vars
+
+try:
+    import os
+except:
+    import uos as os  # unix port compatibility because of missing weaklinks
 
 if platform != "linux":
     import network
@@ -30,74 +36,88 @@ COMPONENT_NAME = "STATS"
 
 _mqtt = config.getMQTT()
 
-RAM_TYPE = '"unit_of_meas":"bytes",' \
-           '"val_tpl":"{{value|int}}",' \
-           '"ic":"mdi:memory",'
-
-CONN_TYPE = '"dev_cla":"connectivity",' \
-            '"pl_on":"online",' \
-            '"pl_off":"offline",'
-
-VERSION_TYPE = '"ic":"mdi:language-python-text",'
+STATE_TYPE = '"json_attributes_topic":"~",' \
+             '"unit_of_meas":"dBm",' \
+             '"val_tpl":"{{value_json.RSSI|int}}",' \
+             '"ic":"mdi:information-outline",'
 
 
 class STATS(Component):
     def __init__(self):
         super().__init__(COMPONENT_NAME, __version__)
         self._interval = config.INTERVAL_SEND_SENSOR
-        asyncio.get_event_loop().create_task(self._loop())
+        self._last_boot = None
 
     async def _init_network(self):
         await super()._init_network()
-        await _mqtt.publish(_mqtt.getDeviceTopic("version"), config.VERSION, qos=1, retain=True)
+        await self._publish()
+        asyncio.get_event_loop().create_task(self._loop())
+        # start loop once network is completely set up because it doesn't have
+        # any use otherwise because component only publishes stats.
+
+    async def _publish(self):
+        val = {}
+        if platform != "linux":
+            sta = network.WLAN(network.STA_IF)
+        else:
+            sta = None
+        val["Pysmartnode version"] = config.VERSION
         if config.RTC_SYNC_ACTIVE is True:
-            for _ in range(5):
-                if time.localtime()[0] == 2000:  # not synced
-                    await asyncio.sleep(1)
-            t = time.localtime()  # polling earlier might not have synced.
-            await _mqtt.publish(_mqtt.getDeviceTopic("last_boot"),
-                                "{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(t[0], t[1], t[2],
-                                                                               t[3],
-                                                                               t[4],
-                                                                               t[5]), qos=1,
-                                retain=True)
+            if self._last_boot is None:
+                for _ in range(5):
+                    if time.localtime()[0] == 2000:  # not synced
+                        await asyncio.sleep(1)
+                t = time.time()  # polling earlier might not have synced.
+                s = round(time.ticks_ms() / 1000)
+                self._last_boot = time.localtime(t - s)  # last real boot/reset, not soft reset
+            t = self._last_boot
+            val["Last boot"] = "{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(t[0], t[1], t[2],
+                                                                              t[3], t[4], t[5])
+            s = int(time.time() - time.mktime(t))
+            m, s = divmod(s, 60)
+            h, m = divmod(m, 60)
+            d, h = divmod(h, 24)
+            val["Uptime"] = '{:d}T{:02d}:{:02d}:{:02d}'.format(d, h, m, s)
+        logging.getLogger("RAM").info(gc.mem_free(), local_only=True)
+        val["RAM free (bytes)"] = gc.mem_free()
+        if sta is not None:
+            try:
+                val["RSSI"] = sta.status("rssi")
+            except Exception as e:
+                val["RSSI"] = 0  # platform doesn't support reading rssi
+                print(e)
+            try:
+                val["IPAddress"] = sta.ifconfig()[0]
+            except Exception as e:
+                print(e)
+                pass
+        else:
+            val["RSSI"] = 0  # can't read rssi on unix port, might not even be connected by WLAN
+        try:
+            val["Device"] = os.uname().sysname
+            val["Micropython version"] = os.uname().version
+        except:
+            pass
+        s = int(_mqtt.getDowntime())
+        m, s = divmod(s, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        val["MQTT Downtime"] = '{:d}T{:02d}:{:02d}:{:02d}'.format(d, h, m, s)
+        val["MQTT Reconnects"] = _mqtt.getReconnects()
+        val["Asyncio waitq"] = "{!s}/{!s}".format(len(asyncio.get_event_loop().waitq),
+                                                  config.LEN_ASYNC_QUEUE)
+        await _mqtt.publish(_mqtt.getDeviceTopic("status"), val, qos=1, retain=False, timeout=5)
 
     async def _loop(self):
         await asyncio.sleep(20)
-        if platform != "linux":
-            s = network.WLAN(network.STA_IF)
-        else:
-            s = None
         while True:
             gc.collect()
-            logging.getLogger("RAM").info(gc.mem_free(), local_only=True)
-            await config.getMQTT().publish(_mqtt.getDeviceTopic("ram_free"), gc.mem_free(),
-                                           timeout=10)
-            if s is not None:
-                try:
-                    await config.getMQTT().publish(_mqtt.getDeviceTopic("rssi"), s.status("rssi"),
-                                                   timeout=10)
-                except Exception as e:
-                    await logging.getLogger("STATS").asyncLog("error",
-                                                              "Error checking rssi: {!s}".format(
-                                                                  e))
-                    s = None
+            await self._publish()
             await asyncio.sleep(self._interval)
 
     async def _discovery(self):
-        await self._publishDiscovery("sensor", _mqtt.getDeviceTopic("ram_free"), "ram_free",
-                                     RAM_TYPE, "RAM free")
-        await self._publishDiscovery("binary_sensor", _mqtt.getDeviceTopic("status"), "status",
-                                     CONN_TYPE, "Status")
-        await self._publishDiscovery("sensor", _mqtt.getDeviceTopic("last_boot"), "last_boot",
-                                     TIMELAPSE_TYPE,
-                                     "Last Boot")
-        await self._publishDiscovery("sensor", _mqtt.getDeviceTopic("version"), "sw_version",
-                                     VERSION_TYPE,
-                                     "SW-Version")
-        if platform != "linux":
-            sens = DISCOVERY_SENSOR.format("signal_strength",  # device_class
-                                           "dB",  # unit_of_measurement
-                                           "{{value|int}}")  # value_template
-            await self._publishDiscovery("sensor", _mqtt.getDeviceTopic("rssi"), "rssi", sens,
-                                         "Signal Strength")
+        topic = _mqtt.getRealTopic(_mqtt.getDeviceTopic("status"))
+        await self._publishDiscovery("sensor", topic, "status", STATE_TYPE,
+                                     "Status {!s}".format(
+                                         config.DEVICE_NAME or sys_vars.getDeviceID()))
+        gc.collect()
