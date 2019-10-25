4c1fe4f339220a6f123b456bedb6400bda226738
Kevin Köck <kevinkk525@users.noreply.github.com>
2019-10-25 19:07:10 +0200

    Extended logging function asyncLog to support timeouts so it can be used in functions that have to be non-blocking if network fails without creating new asyncio tasks for logging.

--- pysmartnode/logging/logging_full.py
+++ pysmartnode/logging/logging_full.py
@@ -4,8 +4,8 @@ Created on 19.07.2017
 @author: Kevin Köck
 '''
 
-__updated__ = "2018-09-28"
-__version__ = "2.5"
+__updated__ = "2019-10-25"
+__version__ = "2.6"
 
 # TODO: Add possibility to use real logging module on esp32_lobo and save logs locally or to sdcard
 
@@ -18,29 +18,35 @@ gc.collect()
 import time
 
 
-async def asyncLog(name, message, level):
+async def asyncLog(name, message, level, timeout=None, await_connection=True):
     if config.getMQTT() is not None:
         base_topic = "{!s}/log/{!s}/{!s}".format(config.MQTT_HOME, "{!s}", sys_vars.getDeviceID())
         # if level is before id other clients can subscribe to e.g. all critical logs
-        await config.getMQTT().publish(base_topic.format(level), "[{!s}] {}".format(name, message), qos=1)
+        await config.getMQTT().publish(base_topic.format(level), "[{!s}] {}".format(name, message),
+                                       qos=1, timeout=timeout, await_connection=await_connection)
     else:
         print(level, message)
 
 
-def log(name, message, level, local_only=False, return_only=False):
+def log(name, message, level, local_only=False, return_only=False, timeout=None):
     if hasattr(config, "RTC_SYNC_ACTIVE") and config.RTC_SYNC_ACTIVE:
         if hasattr(time, "strftime"):
-            print("[{}] [{!s}] [{!s}] {}".format(time.strftime("%Y-%m-%d %H:%M:%S"), name, level, message))
+            print("[{}] [{!s}] [{!s}] {}".format(time.strftime("%Y-%m-%d %H:%M:%S"), name, level,
+                                                 message))
         else:
             t = time.localtime()
-            print("[{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}] [{!s}] [{!s}] {}".format(t[0], t[1], t[2], t[3], t[4],
-                                                                                    t[5], name, level, message))
+            print(
+                "[{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}] [{!s}] [{!s}] {}".format(t[0], t[1], t[2],
+                                                                                  t[3], t[4],
+                                                                                  t[5], name,
+                                                                                  level, message))
     else:
         print("[{!s}] [{!s}] {}".format(name, level, message))
     if return_only:
         return
     if local_only is False:
-        asyncio.get_event_loop().create_task(asyncLog(name, message, level))
+        asyncio.get_event_loop().create_task(asyncLog(name, message, level, timeout=timeout,
+                                                      await_connection=True))
 
 
 class Logger:
@@ -48,23 +54,23 @@ class Logger:
         self.name = name
 
     def critical(self, message, local_only=False):
-        log(self.name, message, "critical", local_only)
+        log(self.name, message, "critical", local_only, timeout=None)
 
     def error(self, message, local_only=False):
-        log(self.name, message, "error", local_only)
+        log(self.name, message, "error", local_only, timeout=None)
 
     def warn(self, message, local_only=False):
-        log(self.name, message, "warn", local_only)
+        log(self.name, message, "warn", local_only, timeout=None)
 
     def info(self, message, local_only=False):
-        log(self.name, message, "info", local_only)
+        log(self.name, message, "info", local_only, timeout=20)
 
     def debug(self, message, local_only=False):
-        log(self.name, message, "debug", local_only)
+        log(self.name, message, "debug", local_only, timeout=5)
 
-    async def asyncLog(self, level, message):
+    async def asyncLog(self, level, message, timeout=None, await_connection=True):
         log(self.name, message, level, return_only=True)
-        await asyncLog(self.name, message, level)
+        await asyncLog(self.name, message, level, timeout, await_connection)
 
 
 def getLogger(name):
--- pysmartnode/logging/logging_light.py
+++ pysmartnode/logging/logging_light.py
@@ -4,8 +4,8 @@ Created on 10.03.2018
 @author: Kevin Köck
 '''
 
-__updated__ = "2018-09-28"
-__version__ = "2.2"
+__updated__ = "2019-10-25"
+__version__ = "2.3"
 
 import gc
 from pysmartnode.utils import sys_vars
@@ -18,34 +18,38 @@ gc.collect()
 class Logging:
     def __init__(self):
         self.id = sys_vars.getDeviceID()
-        self.base_topic = "{!s}/log/{!s}/{!s}".format(config.MQTT_HOME, "{!s}", sys_vars.getDeviceID())
+        self.base_topic = "{!s}/log/{!s}/{!s}".format(config.MQTT_HOME, "{!s}",
+                                                      sys_vars.getDeviceID())
         # if level is before id other clients can subscribe to e.g. all critical logs
 
-    def _log(self, message, level, local_only=False):
+    def _log(self, message, level, local_only=False, timeout=None):
         print("[{!s}] {}".format(level, message))
         if config.getMQTT() is not None and local_only is False:
             asyncio.get_event_loop().create_task(
-                config.getMQTT().publish(self.base_topic.format(level), "{}".format(message)), qos=1)
+                config.getMQTT().publish(self.base_topic.format(level), "{}".format(message),
+                                         qos=1, timeout=timeout, await_connection=True))
 
     def critical(self, message, local_only=False):
-        self._log(message, "critical", local_only)
+        self._log(message, "critical", local_only, timeout=None)
 
     def error(self, message, local_only=False):
-        self._log(message, "error", local_only)
+        self._log(message, "error", local_only, timeout=None)
 
     def warn(self, message, local_only=False):
-        self._log(message, "warn", local_only)
+        self._log(message, "warn", local_only, timeout=None)
 
     def info(self, message, local_only=False):
-        self._log(message, "info", local_only)
+        self._log(message, "info", local_only, timeout=20)
 
     def debug(self, message, local_only=False):
-        self._log(message, "debug", local_only)
+        self._log(message, "debug", local_only, timeout=5)
 
-    async def asyncLog(self, level, message):
+    async def asyncLog(self, level, message, timeout=None, await_connection=True):
         print("[{!s}] {}".format(level, message))
         if config.getMQTT() is not None:
-            await config.getMQTT().publish(self.base_topic.format(level), "{}".format(message), qos=1)
+            await config.getMQTT().publish(self.base_topic.format(level), "{}".format(message),
+                                           qos=1, timeout=timeout,
+                                           await_connection=await_connection)
 
 
 log = Logging()
