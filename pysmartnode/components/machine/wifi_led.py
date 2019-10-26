42f99dd15466c1efbb3949894c37bd4c8ba8c3e9
Kevin Köck <kevinkk525@users.noreply.github.com>
2019-10-26 18:19:25 +0200

    wifi_led major update

--- pysmartnode/components/machine/wifi_led.py
+++ pysmartnode/components/machine/wifi_led.py
@@ -8,8 +8,8 @@ Best to be activated in config.py so it can display the status before receving/l
 Therefore no example configuration given.
 """
 
-__updated__ = "2019-09-29"
-__version__ = "1.2"
+__updated__ = "2019-10-26"
+__version__ = "1.3"
 
 import gc
 import machine
@@ -30,53 +30,42 @@ class WIFILED(Component):
         super().__init__(COMPONENT_NAME, __version__)
         self.pin = Pin(pin, machine.Pin.OUT, value=0 if active_high else 1)
         self._active_high = active_high
-        self.lock = config.Lock()
+        self._next = []
         asyncio.get_event_loop().create_task(self._loop())
-        # discovery although not used could block if no network,
-        # mqtt not needed but will log that this component is used
-
-    async def _init_network(self):
-        await super()._init_network()
 
     async def _loop(self):
         mqtt = config.getMQTT()
         mqtt.registerWifiCallback(self._wifiChanged)
         mqtt.registerConnectedCallback(self._reconnected)
-        await self.async_flash(500, 1)
-        await asyncio.sleep(2)
+        await self._flash(500, 1)
         sta = network.WLAN(network.STA_IF)
+        st = time.ticks_ms()
         while True:
-            while sta.isconnected() is True:
-                await self.async_flash(20, 1)
+            while self._next:
+                await self._flash(*self._next.pop(0))
+                await asyncio.sleep(1)
+            if time.ticks_diff(time.ticks_ms(), st) > 60000:  # heartbeat
                 st = time.ticks_ms()
-                while time.ticks_diff(time.ticks_ms(), st) < 30000:
-                    await asyncio.sleep(1)
-            await asyncio.sleep(5)
-            # to let wifi subscription blink first and wifi reconnect if it was just a brief outage
-            while sta.isconnected() is False:
-                await self.async_flash(500, 3)
-                await asyncio.sleep(5)
+                if sta.isconnected():
+                    await self._flash(20, 1)
+                    await asyncio.sleep_ms(250)
+                    await self._flash(20, 1)
+                else:
+                    await self._flash(500, 3)
+            await asyncio.sleep_ms(500)
 
-    def flash(self, duration, iters):
+    async def _flash(self, duration, iters):
         for _ in range(iters):
             self.pin.value(1 if self._active_high else 0)
-            time.sleep_ms(duration)
+            await asyncio.sleep_ms(duration)
             self.pin.value(0 if self._active_high else 1)
-            time.sleep_ms(duration)
-
-    async def async_flash(self, duration, iters):
-        async with self.lock:
-            for _ in range(iters):
-                self.pin.value(1 if self._active_high else 0)
-                await asyncio.sleep_ms(duration)
-                self.pin.value(0 if self._active_high else 1)
-                await asyncio.sleep_ms(duration)
+            await asyncio.sleep_ms(duration)
 
     async def _wifiChanged(self, state):
         if state is True:
-            await self.async_flash(50, 5)
+            self._next.append((50, 2))
         else:
-            await self.async_flash(500, 5)
+            self._next.append((500, 3))
 
     async def _reconnected(self, client):
-        await self.async_flash(300, 2)
+        self._next.append((100, 5))
