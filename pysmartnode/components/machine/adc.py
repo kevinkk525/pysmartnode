4e04ccd72599f508556ed7dd7998704df83d9f2b
Kevin Köck <kevinkk525@users.noreply.github.com>
2020-03-28 21:54:57 +0100

    ADC calibration support
    ADC now supports calibration values. U=adc/resolution*calibration_v_max+calibration_offset
    This also allows the usage of ATTEN on ESP32 as well as custom adc->voltage conversion e.g. when using a voltage divider.

--- pysmartnode/components/machine/adc.py
+++ pysmartnode/components/machine/adc.py
@@ -8,16 +8,19 @@ example config:
     package: .machine.adc
     component: ADC
     constructor_args: {
-        pin: 0              # ADC pin number or ADC object (even Amux pin object)  
-        atten: None          # optional, attn value to use. Voltages aren't adapted to this config!   
+        pin: 0              # ADC pin number or ADC object (even Amux pin object)
+        # calibration_v_max: 3.3   # optional, v_max for calibration of bad ADC sensors
+        # calibration_offset: 0   # optional, voltage offset for calibration of bad ADC sensors
+        # atten: null          # optional, attn value to use. Voltages aren't adapted to this config, set the calibration kwargs for it to work
+        # max_voltage: 3.3      # optional, defaults to 3.3V, can be given to set max voltage of atten value
     }
 }
 Does not publish anything, just unifies reading of esp8266 ADC, esp32, Amux, Arudino, etc
 You can pass any ADC object or pin number to ADC() and it will return a corretly subclassed pyADC object
 """
 
-__version__ = "1.2"
-__updated__ = "2020-03-19"
+__version__ = "1.5"
+__updated__ = "2020-03-26"
 
 import machine
 from sys import platform
@@ -28,8 +31,11 @@ class pyADC:
     Just a base class to identify all instances of an ADC object sharing the same API
     """
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, *args, calibration_v_max=3.3, calibration_offset=0, max_voltage=3.3,
+                 **kwargs):
+        self._cvm = calibration_v_max
+        self._co = calibration_offset
+        self._mv = max_voltage
 
     def readRaw(self) -> int:
         # just loboris fork compatibility although support officialy dropped.
@@ -38,23 +44,36 @@ class pyADC:
             return self.read() if platform != "esp32_Lobo" else self.readraw()
         return self.read()  # on non-hardware ADCs read() always returns raw values
 
+    def convertToVoltage(self, raw):
+        if platform == "esp8266":
+            v = raw / 1023 * self._cvm + self._co
+        elif platform == "esp32":
+            v = raw / 4095 * self._cvm + self._co
+        elif platform == "esp32_LoBo":
+            v = raw / 1000 + self._co  # loboris fork returns mV
+        else:
+            v = raw / 65535 * self._cvm + self._co  # every platform now provides this method
+        if v > self._mv:
+            return self._mv
+        elif v < 0:
+            return 0.0
+        else:
+            return v
+
     def readVoltage(self) -> float:
         """
         Return voltage according to used platform. Atten values are not recognized
         :return: float
         """
-        if platform == "esp8266":
-            return self.read() / 1023 * 3.3
-        elif platform == "esp32":
-            return self.read() / 4095 * 3.3
-        elif platform == "esp32_LoBo":
-            return self.read() / 1000  # loboris fork returns mV
+        if platform in ("esp8266", "esp32", "esp32_LoBo"):
+            raw = self.read()
         else:
             try:
-                return self.read_u16() / 65535 * 3.3  # every platform should now provide this method
+                raw = self.read_u16()  # every platform should now provide this method
             except NotImplementedError:
                 raise NotImplementedError(
                     "Platform {!s} not implemented, please report".format(platform))
+        return self.convertToVoltage(raw)
 
     def __str__(self):
         return "pyADC generic instance"
@@ -84,10 +103,12 @@ class pyADC:
 
 # machineADC = type("ADC", (machine.ADC, pyADC), {})  # machine.ADC subclass
 class machineADC(machine.ADC, pyADC):
+    # machine.Pin ignores additional kwargs in constructor
     pass
 
 
-def ADC(pin, atten=None, *args, **kwargs) -> pyADC:
+def ADC(pin, *args, atten=None, calibration_v_max=3.3, calibration_offset=0, max_voltage=3.3,
+        **kwargs) -> pyADC:
     if type(pin) == str:
         raise TypeError("ADC pin can't be string")
     if isinstance(pin, pyADC):
@@ -114,14 +135,20 @@ def ADC(pin, atten=None, *args, **kwargs) -> pyADC:
                     "Platform {!s} not implemented, str {!s}, {!s}".format(platform, astr, e))
     if type(pin) == int:
         if platform == "esp32" or platform == "esp32_LoBo":
-            adc = machineADC(machine.Pin(pin), *args, **kwargs)
+            adc = machineADC(machine.Pin(pin), *args, calibration_v_max=calibration_v_max,
+                             calibration_offset=calibration_offset, max_voltage=max_voltage,
+                             **kwargs)
             adc.atten(adc.ATTN_11DB if atten is None else atten)
             return adc
         elif platform == "esp8266":
-            return machineADC(pin, *args, **kwargs)  # esp8266 does not require a pin object
+            return machineADC(pin, *args, calibration_v_max=calibration_v_max,
+                              calibration_offset=calibration_offset, max_voltage=max_voltage,
+                              **kwargs)  # esp8266 does not require a pin object
         else:
             try:
-                return machineADC(machine.Pin(pin), *args, **kwargs)
+                return machineADC(machine.Pin(pin), *args, calibration_v_max=calibration_v_max,
+                                  calibration_offset=calibration_offset, max_voltage=max_voltage,
+                                  **kwargs)
             except Exception as e:
                 raise NotImplementedError(
                     "Platform {!s} not implemented, please report. Fallback resulted in {!s}".format(
