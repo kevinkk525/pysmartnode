9e5c4569c862db41362346b75f7e5ef37f911269
Kevin Köck <kevinkk525@users.noreply.github.com>
2020-04-04 22:44:16 +0200

    [ComponentSwitch] update according to new kwargs handling in constructor, lock optimizations

--- pysmartnode/utils/component/switch.py
+++ pysmartnode/utils/component/switch.py
@@ -2,10 +2,10 @@
 # Copyright Kevin Köck 2019-2020 Released under the MIT license
 # Created on 2019-09-10 
 
-__updated__ = "2020-03-29"
-__version__ = "1.5"
+__updated__ = "2020-04-01"
+__version__ = "1.6"
 
-from pysmartnode.utils.component import Component
+from pysmartnode.utils.component import ComponentBase
 from .definitions import DISCOVERY_SWITCH
 from pysmartnode import config
 import gc
@@ -14,20 +14,17 @@ import uasyncio as asyncio
 _mqtt = config.getMQTT()
 
 
-class ComponentSwitch(Component):
+class ComponentSwitch(ComponentBase):
     """
     Generic Switch class.
     Use it according to the template.
     """
 
-    def __init__(self, component_name, version, unit_index: int, command_topic=None,
-                 instance_name=None, wait_for_lock=True, discover=True, restore_state=True,
+    def __init__(self, component_name, version, unit_index: int, mqtt_topic: str = None,
+                 instance_name=None, wait_for_lock=True, restore_state=True,
                  friendly_name=None, initial_state=None, **kwargs):
         """
-        :param component_name: name of the component that is subclassing this switch (used for discovery and topics)
-        :param version: version of the component module. will be logged over mqtt
-        :param unit_index: counter of the registerd unit of this sensor_type (used for default topics)
-        :param command_topic: command_topic of subclass which controls the switch state. optional.
+        :param mqtt_topic: topic of subclass which controls the switch state. optional. If not ending with "/set", it will be added as the command_topic is being stored.
         :param instance_name: name of the instance. If not provided will get composed of component_name<count>
         :param wait_for_lock: if True then every request waits for the lock to become available,
         :param restore_state: restore the retained state topic state
@@ -36,16 +33,14 @@ class ComponentSwitch(Component):
         :param friendly_name: friendly name for homeassistant gui
         :param initial_state: intitial state of the switch. By default unknown so first state change request will set initial state.
         """
-        super().__init__(component_name, version, unit_index, discover=discover, **kwargs)
-        # discover: boolean, if this component should publish its mqtt discovery.
-        # This can be used to prevent combined Components from exposing underlying
-        # hardware components like a power switch
-
+        super().__init__(component_name, version, unit_index, **kwargs)
         self._state = initial_state  # initial state is unknown if None
-        self._topic = command_topic or _mqtt.getDeviceTopic(
+        if mqtt_topic and not mqtt_topic.endswith("/set"):
+            mqtt_topic = "{}{}".format(mqtt_topic, "/set")
+        self._topic = mqtt_topic or _mqtt.getDeviceTopic(
             "{!s}{!s}/set".format(component_name, self._count))
         _mqtt.subscribeSync(self._topic, self.on_message, self, check_retained_state=restore_state)
-        self.lock = asyncio.Lock()
+        self._lock = asyncio.Lock()
         # in case switch activates a device that will need a while to finish
         self._wfl = wait_for_lock
         self._name = instance_name
@@ -63,7 +58,7 @@ class ComponentSwitch(Component):
             self._event = asyncio.Event()
         return self._event
 
-    def _setState(self, state):
+    def _setState(self, state: bool):
         if state != self._state and self._event is not None:
             self._event.set()
         self._state = state
@@ -83,48 +78,60 @@ class ComponentSwitch(Component):
             raise TypeError("Payload {!s} not supported".format(msg))
         return False  # will not publish the requested state to mqtt as already done by on()/off()
 
-    async def __publish(self, msg):
+    async def _publish(self, msg):
         await _mqtt.publish(self._topic[:-4], msg, qos=1, retain=True)
         self._pub_task = None
 
-    async def on(self):
+    async def on(self) -> bool:
         """Turn switch on. Can be used by other components to control this component"""
-        if self.lock.locked() is True and self._wfl is False:
+        if self._lock.locked() and not self._wfl:
             return False
-        async with self.lock:
-            res = await self._on()  # if _on() returns True the value should be published
-            if res is True:
-                self._setState(True)
-                if self._pub_task:
-                    self._pub_task.cancel()
-                    self._pub_task = None
-                self._pub_task = asyncio.create_task(self.__publish("ON"))
-            return res
+        async with self._lock:
+            return await self.__on()
 
-    async def off(self):
+    async def __on(self) -> bool:
+        res = await self._on()  # if _on() returns True the value should be published
+        if res is True:
+            self._setState(True)
+            if self._pub_task:
+                self._pub_task.cancel()
+                self._pub_task = None
+            self._pub_task = asyncio.create_task(self._publish("ON"))
+        return res
+
+    async def off(self) -> bool:
         """Turn switch off. Can be used by other components to control this component"""
-        if self.lock.locked() is True and self._wfl is False:
+        if self._lock.locked() and not self._wfl:
             return False
-        async with self.lock:
-            res = await self._off()  # if _off() returns True the value should be published
-            if res is True:
-                self._setState(False)
-                if self._pub_task:
-                    self._pub_task.cancel()
-                    self._pub_task = None
-                self._pub_task = asyncio.create_task(self.__publish("OFF"))
-            return res
+        async with self._lock:
+            return await self.__off()
 
-    async def toggle(self):
+    async def __off(self) -> bool:
+        res = await self._off()  # if _off() returns True the value should be published
+        if res is True:
+            self._setState(False)
+            if self._pub_task:
+                self._pub_task.cancel()
+                self._pub_task = None
+            self._pub_task = asyncio.create_task(self._publish("OFF"))
+        return res
+
+    async def toggle(self) -> bool:
         """Toggle device state. Can be used by other component to control this component"""
-        while self.lock.locked() and self._wfl:  # wait for lock
-            await asyncio.sleep_ms(20)
-        if self._state is True:
-            return await self.off()
-        else:
-            return await self.on()
+        if self._wfl:
+            await self._lock.acquire()
+        elif self._lock.locked() and not self._wfl:
+            return False
+        try:
+            if self._state is True:
+                return await self.off()
+            else:
+                return await self.on()
+        finally:
+            if self._wfl:
+                self._lock.release()
 
-    def state(self):
+    def state(self) -> bool:
         return self._state
 
     async def _discovery(self, register=True):
@@ -137,5 +144,6 @@ class ComponentSwitch(Component):
         # note that _publishDiscovery does expect the state topic
         # but we have the command topic stored.
 
-    def topic(self):
+    def topic(self) -> str:
+        """Returns the topic of the component. Note that it returns the command_topic!"""
         return self._topic
