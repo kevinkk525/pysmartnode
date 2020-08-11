0359302bc5bcef812d6d89e3dab40057c8e2390e
Kevin Köck <kevin.koeck@gmx.net>
2020-08-11 17:52:42 +0200

    esp8266: use manifest and board declaration

--- tools/esp8266/esp8266_get_repository.sh
+++ tools/esp8266/esp8266_get_repository.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-sudo apt-get install make unrar-free autoconf automake libtool gcc g++ gperf \
-    flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python-serial \
+sudo apt install make unrar-free autoconf automake libtool gcc g++ gperf \
+    flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python3-serial \
     sed git unzip bash help2man wget bzip2
 sudo apt install python3-dev python3-pip libtool-bin
 pip3 install rshell esptool
 cd ~/
 git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
 cd esp-open-sdk
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+pip install pyserial
+sed -i 's/GNU bash, version (3\.[1-9]|4)/GNU bash, version ([0-9\.]+)/' ~/esp-open-sdk/crosstool-NG/configure.ac
 make STANDALONE=y
 export PATH=/home/kevin/esp-open-sdk/xtensa-lx106-elf/bin:$PATH
 cd ~/
--- tools/esp8266/esp8266_renew.sh
+++ tools/esp8266/esp8266_renew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-./tools/esp8266/esp8266_sync.sh
-./tools/esp8266/esp8266_remove_hints.sh
-./tools/esp8266/esp8266_build.sh
-./tools/esp8266/esp8266_flash.sh
-./tools/esp8266/esp8266_initialize.sh
+./esp8266_sync.sh
+#./esp8266_remove_hints.sh
+./esp8266_build.sh
+./esp8266_flash.sh
+#./esp8266_initialize.sh
 echo "Done"
